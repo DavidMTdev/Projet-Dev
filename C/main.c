e9899fa76caaf89bf657eae797aef2ddd757ce3b
David Teixeira <david.teixeira31@gmail.com>
2019-02-24 16:13:43 +0100

    modification du main.c

--- C/main.c
+++ C/main.c
@@ -4,24 +4,8 @@
 
 int main()
 {
-    menu();
-    int plante = 1;
-    int *humidite_plante;
-    int *luminosite_plante;
-    int *temperature_plante;
-    int floraison_plante[12] = {};
-    int humidite = 7;
-    int luminosite = 7;
-    int temperature = 7;
-    int mois = 9;
-
-    if (plante){
-        ButomeEnOmbelle(&humidite_plante, &luminosite_plante, &temperature_plante, floraison_plante);
-        Floraison(floraison_plante,mois);
-        Humiditer(&humidite_plante, humidite);
-        Luminositer(&luminosite_plante, luminosite);
-        Temperature(&temperature_plante, temperature);
-    }
+    int plante = 0;
+    menu(plante);
 
     return 0;
 }
