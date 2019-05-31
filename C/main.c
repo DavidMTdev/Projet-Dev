28fe043fb043c56879750cc9ed2237c006787908
David Teixeira <david.teixeira31@gmail.com>
2019-05-31 16:14:56 +0200

    add connect db mysql

--- C/main.c
+++ C/main.c
@@ -1,9 +1,74 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "header.h"
+#include <windows.h>
+#include <MYSQL/mysql.h>
 
 int main()
 {
+
+    //Déclaration du pointeur de structure de type MYSQL
+    MYSQL mysql;
+    //Initialisation de MySQL
+    mysql_init(&mysql);
+    //Options de connexion
+    mysql_options(&mysql,MYSQL_READ_DEFAULT_GROUP,"option");
+
+    //Si la connexion réussie...
+    if(mysql_real_connect(&mysql,"127.0.0.1","root","","projet_dev",0,NULL,0))
+    {
+        //Requête qui sélectionne tout dans ma table scores
+        mysql_query(&mysql, "SELECT * FROM plante");
+
+        //Déclaration des objets
+        MYSQL_RES *result = NULL;
+        MYSQL_ROW row;
+
+        unsigned int i = 0;
+        unsigned int num_champs = 0;
+
+        //On met le jeu de résultat dans le pointeur result
+        result = mysql_use_result(&mysql);
+
+        //On récupère le nombre de champs
+        num_champs = mysql_num_fields(result);
+
+        //Tant qu'il y a encore un résultat ...
+        while ((row = mysql_fetch_row(result)))
+        {
+            //On déclare un pointeur long non signé pour y stocker la taille des valeurs
+            unsigned long *lengths;
+
+            //On stocke ces tailles dans le pointeur
+            lengths = mysql_fetch_lengths(result);
+
+            //On fait une boucle pour avoir la valeur de chaque champs
+            for(i = 0; i < num_champs; i++)
+            {
+               //On ecrit toutes les valeurs
+               printf("[%.*s] ", (int) lengths[i], row[i]);
+            }
+            printf("\n");
+        }
+
+        //Libération du jeu de résultat
+        mysql_free_result(result);
+
+        //Fermeture de MySQL
+        mysql_close(&mysql);
+
+    }
+    else  //Sinon ...
+    {
+        printf("Une erreur s'est produite lors de la connexion à la BDD!");
+    }
+
+    return 0;
+
+
+
+
+
     int plante = 0;
     menu(plante);
 
